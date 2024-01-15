7430f917b1710e4b4a8293db93fcccd768bbb9fd
carolineadicahya <11211026@student.itk.ac.id>
2024-01-15 11:19:37 +0800

    stepper form...

--- src/layouts/form_stepper/Lampiran.jsx
+++ src/layouts/form_stepper/Lampiran.jsx
@@ -4,6 +4,10 @@ import { useNavigate } from "react-router-dom";
 const Lampiran = () => {
   const navigate = useNavigate();
 
+  const handleGetStarted = () => {
+    navigate("/dashboard");
+  };
+
   return (
     <div>
       <ol class="flex items-center w-full mb-4 sm:mb-5">
@@ -112,6 +116,7 @@ const Lampiran = () => {
           </div>
         </div>
         <button
+          onClick={handleGetStarted}
           type="submit"
           class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
           Kirim
